8a424507b78496562c176c0144e9918ec5058237
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-08-16 21:50:43 +0500

    create-file

---
+++ src/app/components/Header/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full'>
+        <nav className='w-full h-[70px]'>
+            <div className="title"></div>
+            <div className="search"></div>
+            <div className="cart"></div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
